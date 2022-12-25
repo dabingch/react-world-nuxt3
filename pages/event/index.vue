b0a47d8fcccf6e5b4415ea4538d9a742a77631c7
Hao Chen <hche8093@uni.sydney.edu.au>
2022-12-25 11:02:08 +1100

    Add error handler

--- pages/event/index.vue
+++ pages/event/index.vue
@@ -1,5 +1,5 @@
 <template>
   <div>
-    <h1>Create an event</h1>
+    <h1>Event Index</h1>
   </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -2,3 +2,12 @@
   <h1>Events</h1>
 </template>
 
+<script>
+export default {
+  head() {
+    return {
+      title: 'Event Listing',
+    }
+  }
+}
+</script>
